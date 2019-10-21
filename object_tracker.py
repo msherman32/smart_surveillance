d627882d9d07edd3d10f79b4509a183289d0b996
Michael Sherman <msherman32@gatech.edu>
2019-10-21 14:12:04 -0400

    got mobilenetssd to work for cars video, but slow FPS

--- object_tracker.py
+++ object_tracker.py
@@ -17,6 +17,8 @@ ap.add_argument("-p", "--prototxt", required=True,
        help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True,
        help="path to Caffe pre-trained model")
+ap.add_argument("-v", "--video", required=True,
+       help="path to Video file")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
        help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
@@ -31,7 +33,13 @@ net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 # initialize the video stream and allow the camera sensor to warmup
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+# vs = VideoStream(src=0).start()
+# Set video to load
+videoPath = args["video"]
+
+# Create a video capture object to read videos
+cap = cv2.VideoCapture(videoPath)
+
 time.sleep(2.0)
 
 fps = FPS().start()
@@ -46,8 +54,14 @@ frame_id = 0
 # loop over the frames from the video stream
 while True:
        # read the next frame from the video stream and resize it
-       frame = vs.read()
-       frame = imutils.resize(frame, width=400)
+#        frame = cap.read()
+       # Read first frame
+       success, frame = cap.read()
+       # quit if unable to read the video file
+       if not success:
+        print('Failed to read video')
+        exit(1)
+       frame = imutils.resize(frame, width=300) #400
 
        # if the frame dimensions are None, grab them
        if W is None or H is None:
@@ -56,11 +70,12 @@ while True:
        # construct a blob from the frame, pass it through the network,
        # obtain our output predictions, and initialize the list of
        # bounding box rectangles
-       blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H),
-               (104.0, 177.0, 123.0))
+#        blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H), (104.0, 177.0, 123.0))
+       blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
        net.setInput(blob)
        detections = net.forward()
        rects = []
+#        print(detections)
 
        # loop over the detections
        for i in range(0, detections.shape[2]):
@@ -71,6 +86,7 @@ while True:
                        # the object, then update the bounding box rectangles list
                        box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
                        rects.append(box.astype("int"))
+                #        print(detections[0,0,i,0:7])
 
                        # draw a bounding box surrounding the object so we can
                        # visualize it
@@ -111,13 +127,15 @@ print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 # print(ct.id_to_frame)
 # todo: figure out some of this stuff:
-print("BEGIN:")
-for k, v in ct.begin.items():
-    print (k, '-->', v)
-print("END:")
-for k, v in ct.end.items():
-    print (k, '-->', v)
+# print("BEGIN:")
+# for k, v in ct.begin.items():
+#     print (k, '-->', v)
+# print("END:")
+# for k, v in ct.end.items():
+#     print (k, '-->', v)
+# net.dumpToFile('static/dump.txt')
+# print(net.dump())
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
-vs.stop()
+# vc.stop()
